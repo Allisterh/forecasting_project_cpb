8a9ae229efaeb894b46df4091aecaa96357ac0ce
Vincent <113456433+vincent-v-p@users.noreply.github.com>
2023-02-27 17:26:10 +0100

    gekke code included

--- R Code/Forecasting/Function_File.R
+++ R Code/Forecasting/Function_File.R
@@ -9,6 +9,21 @@ library(xgboost)
 library(caret)
 library(tidyverse)	
 
+## -- Making Dataframe Stationary --
+make.stationary <- function(df){
+  df_2 = data.frame(matrix(nrow = nrow(df)-1, ncol = ncol(df))) 
+  for (i in 1:ncol(df)) {
+    if (adf.test(df[,i])$p.value > 0.05){
+      df_2[,i] <- diff(df[,i],differences=1)
+    }
+    else {
+      df_2[,i] <- df[-1,i]
+    }
+  }
+  colnames(df_2) <- colnames(df)
+  return(df_2)
+}
+
 ## -- Feature Engineering --
 
 ## - F - Feature matrix of Factors and their lags
