6b7cbf571579a59b80be310b40536c96e0713cb7
nikkivdberg <472123nb@eur.nl>
2023-03-07 10:09:17 +0100

    .

--- R Code/Plotting and Graphs/MC_SecondDraft.R
+++ R Code/Plotting and Graphs/MC_SecondDraft.R
@@ -11,9 +11,10 @@ T <- nrow(regressor_matrix)
 
 # RF SMALL Prediction data
 RF_Small_X <- read.csv("RF_small/RF_Small_X.csv")
-# RF_Small_F <- read.csv("RF_small/RF_Small_F.csv")
-# RF_Small_MAF <- read.csv("RF_small/RF_Small_MAF.csv")
-# RF_Small_MARX <- read.csv("RF_small/RF_Small_MARX.csv")
+RF_Small_F <- read.csv("RF_small/RF_Small_F.csv")
+RF_Small_MAF <- read.csv("RF_small/RF_Small_MAF.csv")
+RF_Small_MARX <- read.csv("RF_small/RF_Small_MARX.csv")
+RF_Small_MARX <- RF_Small_MARX[,2:6]
 RF_Small_X_F <- read.csv("RF_small/RF_Small_X_F.csv")
 RF_Small_X_MAF <- read.csv("RF_small/RF_Small_X_MAF.csv")
 RF_Small_X_MARX <- read.csv("RF_small/RF_Small_X_MARX.csv")
@@ -25,6 +26,23 @@ RF_Small_X_F_MARX <- read.csv("RF_small/RF_Small_X_F_MARX.csv")
 RF_Small_X_MAF_MARX <- read.csv("RF_small/RF_Small_X_MAF_MARX.csv")
 RF_Small_F_MAF_MARX <- read.csv("RF_small/RF_Small_F_MAF_MARX.csv")
 RF_Small_X_F_MAF_MARX <- read.csv("RF_small/RF_Small_X_F_MAF_MARX.csv")
+
+# RF BIG Prediction data
+RF_Big_X <- read.csv("RF_Big/RF_Big_X.csv")[,2:6]
+RF_Big_F <- read.csv("RF_Big/RF_Big_F.csv")[,2:6]
+RF_Big_MAF <- read.csv("RF_Big/RF_Big_MAF.csv")[,2:6]
+RF_Big_MARX <- read.csv("RF_Big/RF_Big_MARX.csv")[,2:6]
+RF_Big_X_F <- read.csv("RF_Big/RF_Big_X_F.csv")[,2:6]
+RF_Big_X_MAF <- read.csv("RF_Big/RF_Big_X_MAF.csv")[,2:6]
+RF_Big_X_MARX <- read.csv("RF_Big/RF_Big_X_MARX.csv")[,2:6]
+RF_Big_F_MAF <- read.csv("RF_Big/RF_Big_F_MAF.csv")[,2:6]
+RF_Big_F_MARX <- read.csv("RF_Big/RF_Big_F_MARX.csv")[,2:6]
+RF_Big_MAF_MARX <- read.csv("RF_Big/RF_Big_MAF_MARX.csv")[,2:6]
+RF_Big_X_F_MAF <- read.csv("RF_Big/RF_Big_X_F_MAF.csv")[,2:6]
+RF_Big_X_F_MARX <- read.csv("RF_Big/RF_Big_X_F_MARX.csv")[,2:6]
+RF_Big_X_MAF_MARX <- read.csv("RF_Big/RF_Big_X_MAF_MARX.csv")[,2:6]
+RF_Big_F_MAF_MARX <- read.csv("RF_Big/RF_Big_F_MAF_MARX.csv")[,2:6]
+RF_Big_X_F_MAF_MARX <- read.csv("RF_Big/RF_Big_X_F_MAF_MARX.csv")[,2:6]
 
 # XGB SMALL Prediction data
 XGB_X_forecast_small <- read.csv("XGB_Predictions_small/XGB_X_forecast_small.csv")
@@ -100,54 +118,105 @@ marginal_function <- function(horizons, y_real, Mf_matrices, F_matrices){
   return(Delta_Rsquared)
 }
 
-## -- Calculating Marginal Effects (Direct RF)--
+## -- Calculating Marginal Effects (RF Small)--
 # F
-Mf_matrices <- list(CPB_Direct_RF_X_F_forecast, CPB_Direct_RF_F_MAF_forecast, CPB_Direct_RF_F_MARX_forecast, CPB_Direct_RF_X_F_MAF_forecast, CPB_Direct_RF_X_F_MARX_forecast, CPB_Direct_RF_F_MAF_MARX_forecast, CPB_Direct_RF_X_F_MAF_MARX_forecast)
-F_matrices <- list(CPB_Direct_RF_X_forecast, CPB_Direct_RF_MAF_forecast, CPB_Direct_RF_MARX_forecast, CPB_Direct_RF_X_MAF_forecast, CPB_Direct_RF_X_MARX_forecast, CPB_Direct_RF_MAF_MARX_forecast, CPB_Direct_RF_X_MAF_MARX_forecast)
+Mf_matrices <- list(RF_Small_X_F, RF_Small_F_MAF, RF_Small_F_MARX, RF_Small_X_F_MAF, RF_Small_X_F_MARX, RF_Small_F_MAF_MARX, RF_Small_X_F_MAF_MARX)
+F_matrices <- list(RF_Small_X, RF_Small_MAF, RF_Small_MARX, RF_Small_X_MAF, RF_Small_X_MARX, RF_Small_MAF_MARX, RF_Small_X_MAF_MARX)
 
-CPB_Direct_RF_F_marginal_Rsquared <- marginal_function(horizons, y_real, Mf_matrices, F_matrices)
+RF_Small_F_marginal_Rsquared <- marginal_function(horizons, y_real, Mf_matrices, F_matrices)
 
 # MAF
-Mf_matrices <- list(CPB_Direct_RF_X_MAF_forecast, CPB_Direct_RF_F_MAF_forecast, CPB_Direct_RF_MAF_MARX_forecast, CPB_Direct_RF_X_F_MAF_forecast, CPB_Direct_RF_X_MAF_MARX_forecast, CPB_Direct_RF_F_MAF_MARX_forecast, CPB_Direct_RF_X_F_MAF_MARX_forecast)
-F_matrices <- list(CPB_Direct_RF_X_forecast, CPB_Direct_RF_F_forecast, CPB_Direct_RF_MARX_forecast, CPB_Direct_RF_X_F_forecast, CPB_Direct_RF_X_MARX_forecast, CPB_Direct_RF_F_MARX_forecast, CPB_Direct_RF_X_F_MARX_forecast)
+Mf_matrices <- list(RF_Small_X_MAF, RF_Small_F_MAF, RF_Small_MAF_MARX, RF_Small_X_F_MAF, RF_Small_X_MAF_MARX, RF_Small_F_MAF_MARX, RF_Small_X_F_MAF_MARX)
+F_matrices <- list(RF_Small_X, RF_Small_F, RF_Small_MARX, RF_Small_X_F, RF_Small_X_MARX, RF_Small_F_MARX, RF_Small_X_F_MARX)
 
-CPB_Direct_RF_MAF_marginal_Rsquared <- marginal_function(horizons, y_real, Mf_matrices, F_matrices)
+RF_Small_MAF_marginal_Rsquared <- marginal_function(horizons, y_real, Mf_matrices, F_matrices)
 
 # MARX
-Mf_matrices <- list(CPB_Direct_RF_X_MARX_forecast, CPB_Direct_RF_MAF_MARX_forecast, CPB_Direct_RF_F_MARX_forecast, CPB_Direct_RF_X_MAF_MARX_forecast, CPB_Direct_RF_F_MAF_MARX_forecast, CPB_Direct_RF_X_F_MAF_MARX_forecast, CPB_Direct_RF_X_F_MARX_forecast)
-F_matrices <- list(CPB_Direct_RF_X_forecast, CPB_Direct_RF_MAF_forecast, CPB_Direct_RF_F_forecast, CPB_Direct_RF_X_MAF_forecast, CPB_Direct_RF_F_MAF_forecast, CPB_Direct_RF_X_F_MAF_forecast, CPB_Direct_RF_X_F_forecast)
+Mf_matrices <- list(RF_Small_X_MARX, RF_Small_MAF_MARX, RF_Small_F_MARX, RF_Small_X_MAF_MARX, RF_Small_F_MAF_MARX, RF_Small_X_F_MAF_MARX, RF_Small_X_F_MARX)
+F_matrices <- list(RF_Small_X, RF_Small_MAF, RF_Small_F, RF_Small_X_MAF, RF_Small_F_MAF, RF_Small_X_F_MAF, RF_Small_X_F)
 
-CPB_Direct_RF_MARX_marginal_Rsquared <- marginal_function(horizons, y_real, Mf_matrices, F_matrices)
+RF_Small_MARX_marginal_Rsquared <- marginal_function(horizons, y_real, Mf_matrices, F_matrices)
 
-## -- Plotting Marginal Effects (Direct RF) --
-Marginal_allhorizons_RF_CPB_direct <- data.frame(matrix(ncol = 6, nrow = 3*length(Mf_matrices)))
-Marginal_allhorizons_RF_CPB_direct[,1] <- t(cbind(t(rep("F",length(Mf_matrices))), t(rep("MAF",length(Mf_matrices))), t(rep("MARX",length(Mf_matrices)))))
-colnames(Marginal_allhorizons_RF_CPB_direct) <- c("Feature", "h3", "h6", "h12", "h18", "h24")
+## -- Plotting Marginal Effects (RF Small) --
+Marginal_allhorizons_RF_Small <- data.frame(matrix(ncol = 6, nrow = 3*length(Mf_matrices)))
+Marginal_allhorizons_RF_Small[,1] <- t(cbind(t(rep("F",length(Mf_matrices))), t(rep("MAF",length(Mf_matrices))), t(rep("MARX",length(Mf_matrices)))))
+colnames(Marginal_allhorizons_RF_Small) <- c("Feature", "h3", "h6", "h12", "h18", "h24")
 
 for (f in 1:length(horizons)){
-  Marginal_allhorizons_RF_CPB_direct[,f+1] <- t(cbind(t(CPB_Direct_RF_F_marginal_Rsquared[,f]), t(CPB_Direct_RF_MAF_marginal_Rsquared[,f]), t(CPB_Direct_RF_MARX_marginal_Rsquared[,f])))
+  Marginal_allhorizons_RF_Small[,f+1] <- t(cbind(t(RF_Small_F_marginal_Rsquared[,f]), t(RF_Small_MAF_marginal_Rsquared[,f]), t(RF_Small_MARX_marginal_Rsquared[,f])))
 }
-Marginal_allhorizons_RF_CPB_direct$Feature <- as.factor(Marginal_allhorizons_RF_CPB_direct$Feature)
+Marginal_allhorizons_RF_Small$Feature <- as.factor(Marginal_allhorizons_RF_Small$Feature)
 
 # - Grid Arrange -  
 colors = c("F"= "#845EC2", "MAF" = "#00C9A7", "MARX" = "#009EFA")
 size_title <- 10
 
-ggp1 <- ggplot(Marginal_allhorizons_RF_CPB_direct, aes(x=Feature, y=h3, color=Feature)) + geom_boxplot() + 
+ggp1 <- ggplot(Marginal_allhorizons_RF_Small, aes(x=Feature, y=h3, color=Feature)) + geom_boxplot() + 
   coord_flip() + theme(legend.position = "none") + theme(plot.title = element_text(hjust=0.5, size=size_title), axis.title.x=element_blank(), axis.title.y=element_blank(), 
                                                          axis.text.y=element_blank())+ggtitle("h=3") + scale_fill_manual(values=colors)
 
-ggp2 <- ggplot(Marginal_allhorizons_RF_CPB_direct, aes(x=Feature, y=h6, color=Feature)) + geom_boxplot() + 
+ggp2 <- ggplot(Marginal_allhorizons_RF_Small, aes(x=Feature, y=h6, color=Feature)) + geom_boxplot() + 
   guides(fill=FALSE) + coord_flip() + theme(legend.position = "none") + theme(plot.title = element_text(hjust=0.5, size=size_title),axis.title.x=element_blank(), axis.title.y=element_blank(), axis.text.y=element_blank())+ggtitle("h=6")
 
-ggp3 <- ggplot(Marginal_allhorizons_RF_CPB_direct, aes(x=Feature, y=h12, color=Feature)) + geom_boxplot() + 
+ggp3 <- ggplot(Marginal_allhorizons_RF_Small, aes(x=Feature, y=h12, color=Feature)) + geom_boxplot() + 
   guides(fill=FALSE) + coord_flip() + theme(legend.position = "none") + theme(plot.title = element_text(hjust=0.5, size=size_title),axis.title.x=element_blank(), axis.title.y=element_blank(), axis.text.y=element_blank())+ggtitle("h=12")
 
-ggp4 <- ggplot(Marginal_allhorizons_RF_CPB_direct, aes(x=Feature, y=h18, color=Feature)) + geom_boxplot() + 
+ggp4 <- ggplot(Marginal_allhorizons_RF_Small, aes(x=Feature, y=h18, color=Feature)) + geom_boxplot() + 
   guides(fill=FALSE) + coord_flip() + theme(legend.position = "none") + theme(plot.title = element_text(hjust=0.5, size=size_title),axis.title.x=element_blank(), axis.title.y=element_blank(), axis.text.y=element_blank())+ggtitle("h=18")
 
-ggp5 <- ggplot(Marginal_allhorizons_RF_CPB_direct, aes(x=Feature, y=h24, color=Feature)) + geom_boxplot() + 
-  guides(fill=FALSE) + coord_flip()  + theme(plot.title = element_text(hjust=0.5, size=size_title), axis.title.x=element_blank(), axis.title.y=element_blank(), axis.text.y=element_blank())+ggtitle("h=24")
+ggp5 <- ggplot(Marginal_allhorizons_RF_Small, aes(x=Feature, y=h24, color=Feature)) + geom_boxplot() + 
+  guides(fill=FALSE) + coord_flip() + theme(legend.position = "none") + theme(plot.title = element_text(hjust=0.5, size=size_title), axis.title.x=element_blank(), axis.title.y=element_blank(), axis.text.y=element_blank())+ggtitle("h=24")
+
+grid.arrange(ggp1, ggp2, ggp3, ggp4, ggp5, ncol = 5)
+
+## -- Calculating Marginal Effects (RF BIG)--
+# F
+Mf_matrices <- list(RF_Big_X_F, RF_Big_F_MAF, RF_Big_F_MARX, RF_Big_X_F_MAF, RF_Big_X_F_MARX, RF_Big_F_MAF_MARX, RF_Big_X_F_MAF_MARX)
+F_matrices <- list(RF_Big_X, RF_Big_MAF, RF_Big_MARX, RF_Big_X_MAF, RF_Big_X_MARX, RF_Big_MAF_MARX, RF_Big_X_MAF_MARX)
+
+RF_Big_F_marginal_Rsquared <- marginal_function(horizons, y_real, Mf_matrices, F_matrices)
+
+# MAF
+Mf_matrices <- list(RF_Big_X_MAF, RF_Big_F_MAF, RF_Big_MAF_MARX, RF_Big_X_F_MAF, RF_Big_X_MAF_MARX, RF_Big_F_MAF_MARX, RF_Big_X_F_MAF_MARX)
+F_matrices <- list(RF_Big_X, RF_Big_F, RF_Big_MARX, RF_Big_X_F, RF_Big_X_MARX, RF_Big_F_MARX, RF_Big_X_F_MARX)
+
+RF_Big_MAF_marginal_Rsquared <- marginal_function(horizons, y_real, Mf_matrices, F_matrices)
+
+# MARX
+Mf_matrices <- list(RF_Big_X_MARX, RF_Big_MAF_MARX, RF_Big_F_MARX, RF_Big_X_MAF_MARX, RF_Big_F_MAF_MARX, RF_Big_X_F_MAF_MARX, RF_Big_X_F_MARX)
+F_matrices <- list(RF_Big_X, RF_Big_MAF, RF_Big_F, RF_Big_X_MAF, RF_Big_F_MAF, RF_Big_X_F_MAF, RF_Big_X_F)
+
+RF_Big_MARX_marginal_Rsquared <- marginal_function(horizons, y_real, Mf_matrices, F_matrices)
+
+## -- Plotting Marginal Effects (RF Big) --
+Marginal_allhorizons_RF_Big <- data.frame(matrix(ncol = 6, nrow = 3*length(Mf_matrices)))
+Marginal_allhorizons_RF_Big[,1] <- t(cbind(t(rep("F",length(Mf_matrices))), t(rep("MAF",length(Mf_matrices))), t(rep("MARX",length(Mf_matrices)))))
+colnames(Marginal_allhorizons_RF_Big) <- c("Feature", "h3", "h6", "h12", "h18", "h24")
+
+for (f in 1:length(horizons)){
+  Marginal_allhorizons_RF_Big[,f+1] <- t(cbind(t(RF_Big_F_marginal_Rsquared[,f]), t(RF_Big_MAF_marginal_Rsquared[,f]), t(RF_Big_MARX_marginal_Rsquared[,f])))
+}
+Marginal_allhorizons_RF_Big$Feature <- as.factor(Marginal_allhorizons_RF_Big$Feature)
+
+# - Grid Arrange -  
+colors = c("F"= "#845EC2", "MAF" = "#00C9A7", "MARX" = "#009EFA")
+size_title <- 10
+
+ggp1 <- ggplot(Marginal_allhorizons_RF_Big, aes(x=Feature, y=h3, color=Feature)) + geom_boxplot() + 
+  coord_flip() + theme(legend.position = "none") + theme(plot.title = element_text(hjust=0.5, size=size_title), axis.title.x=element_blank(), axis.title.y=element_blank(), 
+                                                         axis.text.y=element_blank())+ggtitle("h=3") + scale_fill_manual(values=colors)
+
+ggp2 <- ggplot(Marginal_allhorizons_RF_Big, aes(x=Feature, y=h6, color=Feature)) + geom_boxplot() + 
+  guides(fill=FALSE) + coord_flip() + theme(legend.position = "none") + theme(plot.title = element_text(hjust=0.5, size=size_title),axis.title.x=element_blank(), axis.title.y=element_blank(), axis.text.y=element_blank())+ggtitle("h=6")
+
+ggp3 <- ggplot(Marginal_allhorizons_RF_Big, aes(x=Feature, y=h12, color=Feature)) + geom_boxplot() + 
+  guides(fill=FALSE) + coord_flip() + theme(legend.position = "none") + theme(plot.title = element_text(hjust=0.5, size=size_title),axis.title.x=element_blank(), axis.title.y=element_blank(), axis.text.y=element_blank())+ggtitle("h=12")
+
+ggp4 <- ggplot(Marginal_allhorizons_RF_Big, aes(x=Feature, y=h18, color=Feature)) + geom_boxplot() + 
+  guides(fill=FALSE) + coord_flip() + theme(legend.position = "none") + theme(plot.title = element_text(hjust=0.5, size=size_title),axis.title.x=element_blank(), axis.title.y=element_blank(), axis.text.y=element_blank())+ggtitle("h=18")
+
+ggp5 <- ggplot(Marginal_allhorizons_RF_Big, aes(x=Feature, y=h24, color=Feature)) + geom_boxplot() + 
+  guides(fill=FALSE) + coord_flip() + theme(legend.position = "none") + theme(plot.title = element_text(hjust=0.5, size=size_title), axis.title.x=element_blank(), axis.title.y=element_blank(), axis.text.y=element_blank())+ggtitle("h=24")
 
 grid.arrange(ggp1, ggp2, ggp3, ggp4, ggp5, ncol = 5)
 
@@ -253,6 +322,7 @@ ggp5 <- ggplot(Marginal_allhorizons_XGB_big, aes(x=Feature, y=h24, color=Feature
 grid.arrange(ggp1, ggp2, ggp3, ggp4, ggp5, ncol = 5)
 
 ## -- MEDIANS --
+# XGB
 median_xgb_small_h3_F <- median(Marginal_allhorizons_XGB_SMALL[1:7,2])
 median_xgb_small_h3_MAF <- median(Marginal_allhorizons_XGB_SMALL[8:14,2])
 median_xgb_small_h3_MARX <- median(Marginal_allhorizons_XGB_SMALL[15:21,2])
@@ -268,3 +338,20 @@ median_xgb_big_h3_MARX <- median(Marginal_allhorizons_XGB_big[15:21,2])
 median_xgb_big_h24_F <- median(Marginal_allhorizons_XGB_big[1:7,6])
 median_xgb_big_h24_MAF <- median(Marginal_allhorizons_XGB_big[8:14,6])
 median_xgb_big_h24_MARX <- median(Marginal_allhorizons_XGB_big[15:21,6])
+
+# RF
+median_RF_small_h3_F <- median(Marginal_allhorizons_RF_Small[1:7,2])
+median_RF_small_h3_MAF <- median(Marginal_allhorizons_RF_Small[8:14,2])
+median_RF_small_h3_MARX <- median(Marginal_allhorizons_RF_Small[15:21,2])
+
+median_RF_small_h24_F <- median(Marginal_allhorizons_RF_Small[1:7,6])
+median_RF_small_h24_MAF <- median(Marginal_allhorizons_RF_Small[8:14,6])
+median_RF_small_h24_MARX <- median(Marginal_allhorizons_RF_Small[15:21,6])
+
+median_RF_big_h3_F <- median(Marginal_allhorizons_RF_Big[1:7,2])
+median_RF_big_h3_MAF <- median(Marginal_allhorizons_RF_Big[8:14,2])
+median_RF_big_h3_MARX <- median(Marginal_allhorizons_RF_Big[15:21,2])
+
+median_RF_big_h24_F <- median(Marginal_allhorizons_RF_Big[1:7,6])
+median_RF_big_h24_MAF <- median(Marginal_allhorizons_RF_Big[8:14,6])
+median_RF_big_h24_MARX <- median(Marginal_allhorizons_RF_Big[15:21,6])
